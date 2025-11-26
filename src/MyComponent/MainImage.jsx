f8b99212c61be67e7e11c6c1b39bd041cc17f63a
Your Name <your@email.com>
2025-11-26 21:54:17 +0300

    'Message-component'

--- src/MyComponent/MainImage.jsx
+++ src/MyComponent/MainImage.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { sliderImages } from "../Data";
 import TextType from './Welcome/TextType'
 export default function MainImage() {
+   const [currentIndex,setcurrentIndex]=useState(0);
+  useEffect(()=>{
+    const interval= setInterval(() => {
+      setcurrentIndex((prevIndex)=>prevIndex===sliderImages.length -1 ? 0 :prevIndex + 1)
+      
+    }, 4000);
+    return ()=>clearInterval(interval)
+  },[])
+
+
+
+
   return (
+
+
     <div className='bg-[#e8edf6]'>
          <section
-      className="relative w-full h-[90vh] bg-cover bg-center bg-no-repeat"
+      className="relative w-full h-[90vh] bg-cover bg-center bg-no-repeat slider-image"
       style={{
-        backgroundImage: `url(${sliderImages[0]}) ` 
+        backgroundImage: `url(${sliderImages[currentIndex]}) ` 
       }}
     >
-
-
-
-
     </section>
           {/* Content */}
-      <div className="relative z-10  flex flex-col items-center justify-center h-full text-center px-4 bg-white py-2">
+      <div className="relative z-10  flex flex-col items-center justify-center h-full text-center px-4 bg-red-800 py-2">
  <h1 className=' font-extrabold text-2xl md-text-4xl leding-tight'>      
      <TextType 
   text={[" Welcome to Sudarshan Security Service", " Let’s give them a", " elite service!"]}
@@ -24,7 +34,7 @@ export default function MainImage() {
   pauseDuration={1500}
   showCursor={true}
   cursorCharacter="|"
-  className='text-type text-black'
+  className='text-type text-white '
   cursorClassName='text-type__cursor'
 />
 </h1> 
